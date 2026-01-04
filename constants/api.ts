990536faace84f3b7ff050efd04af04861e0846e
turgut5534 <turgutsalgin5534@gmail.com>
2026-01-04 14:46:13 +0100

    API fixed

--- constants/api.ts
+++ constants/api.ts
@@ -4,7 +4,7 @@ export const BASE_URL = 'https://fuelcards.turgutsalgin.com';
 export const API_ENDPOINTS = {
   LOGIN: `${BASE_URL}/auth/login`,
   REGISTER: `${BASE_URL}/auth/register`,
-  PROFILE: `${BASE_URL}/user/profile`,
+  PROFILE: `${BASE_URL}/auth/profile`,
   CHANGE_PASSWORD: `${BASE_URL}/auth/changePassword`,
   CARDS: `${BASE_URL}/cards`,
 };
